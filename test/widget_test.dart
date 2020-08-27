f15b52e0b5ea189c9962ea85cf8aa9bcdb44f290
susumOyaji <sumitomo0210@gmail.com>
2020-08-27 17:44:56 +0900

    up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stockcard/Indicator.dart';
+import 'package:stockcard/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
